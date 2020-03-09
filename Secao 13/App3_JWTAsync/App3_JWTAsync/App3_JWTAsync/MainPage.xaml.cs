999bb42c91d0641215bce44e983df2ca76cd252f
atiliosilfer <atilio.silvaferreira@gmail.com>
2020-03-09 17:37:43 -0300

    App3_JWTAsync - Completo - Aplicação WS JWT

---
+++ Secao 13/App3_JWTAsync/App3_JWTAsync/App3_JWTAsync/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App3_JWTAsync.Service;
+using App3_JWTAsync.Model;
+
+namespace App3_JWTAsync {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private async void GetTokenAction(object sender, EventArgs args) {
+            string resultado = await JWTService.GetToken(nome.Text, password.Text);
+            LblToken.Text = resultado;
+        }
+
+        private async void VerificarAction(object sender, EventArgs args) {
+            string resultado = await JWTService.Verificar();
+            LblResult.Text = resultado;
+        }
+    }
+}
